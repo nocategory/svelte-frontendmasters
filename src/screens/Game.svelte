bbb4b8e4f0521d6a8ab362d2ea69b8441c726bb4
nocategory <me@nocategory.dev>
2021-01-23 19:52:43 +0000

    wiring up interactivity

--- src/screens/Game.svelte
+++ src/screens/Game.svelte
@@ -1,81 +1,98 @@
 <script>
-    import Card from '../components/Card.svelte'
-    export let selection
+  import Card from '../components/Card.svelte'
+  export let selection
 
-    const load_details = async (celeb) => {
-        const res = await fetch(`https://cameo-explorer.netlify.app/celebs/${celeb.id}.json`)
-        return await res.json()
+  const load_details = async (celeb) => {
+    const res = await fetch(
+      `https://cameo-explorer.netlify.app/celebs/${celeb.id}.json`
+    )
+    return await res.json()
+  }
+
+  const promises = selection.map((round) =>
+    Promise.all([load_details(round.a), load_details(round.b)])
+  )
+
+  let i = 0
+
+  const submit = (celeb_a, celeb_b, sign) => {
+    const result =
+      Math.sign(celeb_a.price - celeb_b.price) === sign ? 'right' : 'wrong'
+
+    console.log({ result })
+
+    if (i < selection.length - 1) {
+      i++
+    } else {
+      //end game
     }
-
-    const promises = selection.map(round => Promise.all([
-        load_details(round.a),
-        load_details(round.b)
-    ]))
-
-    let i = 0
+  }
 </script>
 
 <header>
-    <p>Tap on the more monetisable celebrity's face, or tap 'same price' if society values them equally.</p>
+  <p>
+    Tap on the more monetisable celebrity's face, or tap 'same price' if society
+    values them equally.
+  </p>
 </header>
 
 <div class="game-container">
-    {#await promises[i] then [a, b]}
+  {#await promises[i] then [a, b]}
     <div class="game">
-        <div class="card-container">
-            <Card celeb={a} />
-        </div>
+      <div class="card-container">
+        <Card celeb={a} on:select={() => submit(a, b, 1)} />
+      </div>
 
-        <div>
-            <button class="same">
-                same price
-            </button>
-        </div>
+      <div>
+        <button class="same" on:click={() => submit(a, b, 0)}>
+          same price
+        </button>
+      </div>
 
-        <div class="card-container">
-            <Card celeb={b} />
-        </div>
+      <div class="card-container">
+        <Card celeb={b} on:select={() => submit(a, b, -1)} />
+      </div>
     </div>
-    {:catch}
-        <p class="error">Oops! Failed to load</p>
-    {/await}
+  {:catch}
+    <p class="error">Oops! Failed to load</p>
+  {/await}
 </div>
 
 <div class="results">
-    <p>results will go here</p>
+  <p>results will go here</p>
 </div>
 
 <style>
-    .game-container {
-        flex: 1
-    }
+  .game-container {
+    flex: 1;
+  }
+  .game {
+    display: grid;
+    grid-template-rows: 1fr 2em 1fr;
+    grid-gap: 0.5em;
+    width: 100%;
+    height: 100%;
+    max-width: min(100%, 40vh);
+    margin: 0 auto;
+  }
+
+  .game > div {
+    display: flex;
+    align-items: center;
+  }
+  .error {
+    color: red;
+  }
+
+  @media (min-width: 640px) {
     .game {
-        display: grid;
-        grid-template-rows: 1fr 2em 1fr;
-        grid-gap: 0.5em;
-        width: 100%;
-        height: 100%;
-        max-width: min(100%, 40vh);
-        margin: 0 auto;
+      max-width: 100%;
+      grid-template-rows: none;
+      grid-template-columns: 1fr 8em 1fr;
+      max-height: calc(100vh - 6em); /** safari specific */
     }
-
-    .game > div {
-        display: flex;
-        align-items: center;
+    .same {
+      height: 8em;
     }
-    .error {
-        color: red;
-    }
-
-    @media (min-width: 640px) {
-        .game {
-            max-width: 100%;
-            grid-template-rows: none;
-            grid-template-columns: 1fr 8em 1fr;
-            max-height: calc(100vh - 6em); /** safari specific */
-        }
-        .same {
-            height: 8em;
-        }
-    }
+  }
 </style>
